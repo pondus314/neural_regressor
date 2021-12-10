9cca6f34ab5c54cc6ad95a0a969530d2e946a1f5
pondus314 <peter.ondus@gmail.com>
2021-12-10 16:43:20 +0000

    fix input distribution

--- main.py
+++ main.py
@@ -16,7 +16,31 @@ if __name__ == '__main__':
     # operation2 = operations.UnivariateOperation(operations.UnivariateOp.POWER, add_linear_layer=True)
     # grey_box_node_2 = nn_node.GreyBoxNode(operation2, child_nodes=[black_box_node])
 
-    distribution = torch.distributions.normal.Normal(torch.tensor([0.]), torch.tensor([10.]))
-    dataset = generated_dataset.GeneratorDataset(lambda x: x*2, distribution, 20)
-    for (x, y) in dataset:
-        print(x, y)
+    # distribution = torch.distributions.normal.Normal(torch.tensor([0.]), torch.tensor([10.]))
+    # dataset = generated_dataset.GeneratorDataset(lambda x: x*2, distribution, 20)
+    # for (x, y) in dataset:
+    #     print(x, y)
+
+    def f(x0, x1, x2):
+        return x0 ** 2 + x1 * x2
+
+
+    distribution = torch.distributions.normal.Normal(torch.zeros((3,)), torch.ones((3,)))
+    dataset = generated_dataset.GeneratorDataset(f, distribution, 2000)
+
+    leaf_nodes = [nn_node.LeafNode(add_linear_layer=False) for _ in range(3)]
+    pow_operation = operations.UnivariateOperation(operations.UnivariateOp.POWER, False)
+    pow_node = nn_node.GreyBoxNode(operation=pow_operation,
+                                   child_nodes=leaf_nodes[0:1],
+                                   child_input_idxs=None)
+    multi_node = nn_node.GreyBoxNode(
+        operation=operations.MultivariateOperation(operations.MultivariateOp.MULTIPLY, False),
+        child_nodes=leaf_nodes[1:],
+        child_input_idxs={leaf_nodes[1]: [0], leaf_nodes[2]: [1]},
+    )
+    tree = nn_node.GreyBoxNode(
+        operation=operations.MultivariateOperation(operations.MultivariateOp.ADD, False),
+        child_nodes=[pow_node, multi_node],
+        child_input_idxs={pow_node: [0], multi_node: [1, 2]}
+    )
+    print(tree(torch.tensor([[1., 0., 3.]])))
--- nn_node.py
+++ nn_node.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 import torch
 import abc
@@ -35,7 +35,7 @@ class GreyBoxNode(NnNode):
         super(GreyBoxNode, self).__init__()
         self.operation: Operation = operation
         self.child_nodes: List[NnNode] = child_nodes
-        self.child_input_idxs: Dict[NnNode, List[int]] = child_input_idxs
+        self.child_input_idxs: Optional[Dict[NnNode, List[int]]] = child_input_idxs
 
     def forward(self, *inputs) -> torch.Tensor:
         if len(self.child_nodes) == 1:
@@ -45,7 +45,23 @@ class GreyBoxNode(NnNode):
 
         children_outputs = []
         for child in self.child_nodes:
-            child_inputs = torch.cat([inputs[idx] for idx in self.child_input_idxs[child]], 1)
-            children_outputs.append(child(child_inputs))
-        out = self.operation(children_outputs)
+            child_inputs = tuple(inputs[idx] for idx in self.child_input_idxs[child])
+            children_outputs.append(child(*child_inputs))
+        print(children_outputs)
+        out = self.operation(*children_outputs)
         return out
+
+
+class LeafNode(NnNode):
+    def __init__(self, add_linear_layer: bool = True):
+        super(LeafNode, self).__init__()
+        self.add_linear_layer = add_linear_layer
+        if add_linear_layer:
+            self.linear_layer = nn.Linear(1, 1)
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        if inputs.numel() != 1:
+            raise TypeError("input to leaf node has to be a singular variable, received ", inputs)
+        if self.add_linear_layer:
+            inputs = self.linear_layer(inputs)
+        return inputs
