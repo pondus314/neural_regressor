05ee18d3e7eb004214d6edca8fc1b0b0bdc8d911
pondus314 <peter.ondus@gmail.com>
2021-12-17 17:26:14 +0100

    tweak default black box network

--- black_box.py
+++ black_box.py
@@ -4,10 +4,10 @@ from torch import nn
 
 def create_black_box(n_inputs: int) -> nn.Sequential:
     black_box = nn.Sequential(
-        nn.Linear(n_inputs, n_inputs * 2 // 3),
+        nn.Linear(n_inputs, max(8,n_inputs * 2 // 3)),
         nn.ReLU(),
-        nn.Linear(n_inputs * 2 // 3, n_inputs * 2 // 3),
+        nn.Linear(max(8, n_inputs * 2 // 3), max(8, n_inputs * 2 // 3)),
         nn.ReLU(),
-        nn.Linear(n_inputs * 2 // 3, 1),
+        nn.Linear(max(8, n_inputs * 2 // 3), 1),
     )
     return black_box
