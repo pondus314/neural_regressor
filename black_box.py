2f616df62d1078a229fb09028844981aba1dd749
pondus314 <peter.ondus@gmail.com>
2022-01-20 09:53:31 +0000

    change mlp non-linearity to be double differentiable

--- black_box.py
+++ black_box.py
@@ -5,9 +5,9 @@ from torch import nn
 def create_black_box(n_inputs: int) -> nn.Sequential:
     black_box = nn.Sequential(
         nn.Linear(n_inputs, max(8,n_inputs * 2 // 3)),
-        nn.ReLU(),
+        nn.Tanh(),
         nn.Linear(max(8, n_inputs * 2 // 3), max(8, n_inputs * 2 // 3)),
-        nn.ReLU(),
+        nn.Tanh(),
         nn.Linear(max(8, n_inputs * 2 // 3), 1),
     )
     return black_box
