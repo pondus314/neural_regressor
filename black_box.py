ac9f112115701c4d5d466451ee76da34af6710e5
pondus314 <peter.ondus@gmail.com>
2022-02-01 17:12:43 +0000

    reduce redundancy in black box

--- black_box.py
+++ black_box.py
@@ -3,11 +3,12 @@ from torch import nn
 
 
 def create_black_box(n_inputs: int) -> nn.Sequential:
+    layer_size = max(8, n_inputs * 2 // 3)
     black_box = nn.Sequential(
-        nn.Linear(n_inputs, max(8,n_inputs * 2 // 3)),
+        nn.Linear(n_inputs, layer_size),
         nn.Tanh(),
-        nn.Linear(max(8, n_inputs * 2 // 3), max(8, n_inputs * 2 // 3)),
+        nn.Linear(layer_size, layer_size),
         nn.Tanh(),
-        nn.Linear(max(8, n_inputs * 2 // 3), 1),
+        nn.Linear(layer_size, 1),
     )
     return black_box
